4aa0cec8df31e2efcfd0acf20750121c8fb7cb03
Ryan Drachenberg <tssinvestments@gmail.com>
2024-12-17 17:43:26 -0500

    vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 const nextConfig: NextConfig = {
   images: {
@@ -11,6 +12,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+
 };
 
 export default nextConfig;
